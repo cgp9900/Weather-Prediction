7c3323a3191a4dd7e53f3c247a3f9fcc85b3ea17
cgp9900 <cgp9900@gmail.com>
2024-02-08 07:29:57 -0500

    pytest import removal

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,7 +1,10 @@
-import pytest
-
 from weather import weather
 
 
 def test_weather():
     assert weather() == "weather"
+
+
+def test_weather_uppercase():
+    assert weather(True) == "WEATHER"
+    

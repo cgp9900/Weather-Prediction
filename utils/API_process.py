53eab3614692c6b77f0b6f5481fedb46a78591b3
cgp9900 <cgp9900@gmail.com>
2024-02-08 17:17:49 -0500

    attempt to disable long line

--- utils/API_process.py
+++ utils/API_process.py
@@ -5,7 +5,6 @@ import pandas as pd
 def API_process(w_data):
     # Process hourly data. The order of variables needs to be the same as
     # requested.
-    # pylint: disable-next=line-too-long
     hourly = w_data.Hourly()
     hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 
@@ -36,7 +35,7 @@ def API_process(w_data):
     IQR = q3 - q1
 
     hourly_numeric[
-        (hourly_numeric >= (q3 + 1.5 * IQR)) | (hourly_numeric <= (q1 - 1.5 * IQR))
+        (hourly_numeric >= (q3 + 1.5 * IQR)) | (hourly_numeric <= (q1 - 1.5 * IQR)) # pylint: disable=line-too-long
     ] = np.nan
     hourly_numeric.fillna(method="ffill", inplace=True)
 

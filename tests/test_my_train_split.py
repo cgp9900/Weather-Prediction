38a0f08ec4971352c4642c495719063d98938001
cgp9900 <cgp9900@gmail.com>
2024-02-13 08:33:47 -0500

    Adjusted testing files

--- tests/test_my_train_split.py
+++ tests/test_my_train_split.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import pytest
 
 from utils import my_train_split
 
@@ -12,7 +11,7 @@ def test_my_train_split_sorted_dates_X_train():
     assert X_train[::-1][0] == pd.Timestamp("2023-12-09T00", tz="UTC")
 
 
-def test_my_train_split_sorted_dates_X_train():
+def test_my_train_split_sorted_dates_X_test():
     X = pd.date_range("2023-12-01T05:00:00.000Z", "2023-12-10T23:00:00.000Z", freq="h")
     y = np.random.randn(235)
     X_train, X_test, y_train, y_test = my_train_split.my_train_split(X, y)
